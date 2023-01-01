cf2ca2826387d0b3136fe2e3b55df798ae1af84d
Akshit Koduru <koduruakshit2003@gmail.com>
2023-01-01 19:06:10 +0530

    first commit

--- js/flute.js
+++ js/flute.js
@@ -2,51 +2,53 @@ let audioPointer;
 let audio;
 let i=0;
 let txt="";
-
-/*
-const audioArray = [ 
-  "b.mp3",
-  "g1" 
-];
-*/
+//let example="";
 const mus = {
-  A: new Audio("audio/Flute/f1.mp3")
-//   B: new Audio("f2.mp3"),
-//   C: new Audio("f3.mp3"),
-//   D: new Audio("f4.mp3"),
-//   E: new Audio("f5.mp3"),
-//   F: new Audio("f6.mp3"),
-//   G: new Audio("f7.mp3"),
-//   H: new Audio("f8.mp3"),
-//   I: new Audio("f9.mp3"),
-//   J: new Audio("f10.mp3"),
-//   K: new Audio("f11.mp3"),
-//   L: new Audio("f12.mp3"),
-//   M: new Audio("f13.mp3"),
-//   N: new Audio("f14.mp3"),
-//   O: new Audio("f15.mp3"),
-//   P: new Audio("f16.mp3"),
-//   Q: new Audio("f17.mp3"),
-//   R: new Audio("f18.mp3"),
-//   S: new Audio("f19.mp3"),
-//   T: new Audio("f20.mp3"),
-//   U: new Audio("f21.mp3"),
-//   V: new Audio("f22.mp3"),  /*&#119187*/ 
-//   W: new Audio("f23.mp3"),
-//   X: new Audio("f24.mp3"),
-//   Y: new Audio("f25.mp3"), 
-//   Z: new Audio("f26.mp3"),
-//  " ":new Audio("f27.mp3"),   //&#119047;	\x1d107
-//  "#":new Audio("f28.mp3")
+  A: new Audio("audio/Flute/f1.MP3"),
+  B: new Audio("audio/Flute/f2.mp3"),
+  C: new Audio("audio/Flute/f3.mp3"),
+  D: new Audio("audio/Flute/f4.mp3"),
+  E: new Audio("audio/Flute/f5.mp3"),
+  F: new Audio("audio/Flute/f6.mp3"),
+  G: new Audio("audio/Flute/f7.mp3"),
+  H: new Audio("audio/Flute/f8.mp3"),
+  I: new Audio("audio/Flute/f9.mp3"),
+  J: new Audio("audio/Flute/f10.mp3"),
+  K: new Audio("audio/Flute/f11.mp3"),
+  L: new Audio("audio/Flute/f12.mp3"),
+  M: new Audio("audio/Flute/f13.mp3"),
+  N: new Audio("audio/Flute/f14.mp3"),
+  O: new Audio("audio/Flute/f15.mp3"),
+  P: new Audio("audio/Flute/f16.mp3"),
+  Q: new Audio("audio/Flute/f17.mp3"),
+  R: new Audio("audio/Flute/f18.mp3"),
+  S: new Audio("audio/Flute/f19.mp3"),
+  T: new Audio("audio/Flute/f20.mp3"),
+  U: new Audio("audio/Flute/f21.mp3"),
+  V: new Audio("audio/Flute/f22.mp3"),  /*&#119187*/ 
+  W: new Audio("audio/Flute/f23.mp3"),
+  X: new Audio("audio/Flute/f24.mp3"),
+  Y: new Audio("audio/Flute/f25.mp3"), 
+  Z: new Audio("audio/Flute/f26.mp3"),
+ " ":new Audio("audio/Flute/f27.mp3"),   //&#119047;	\x1d107
+ "#":new Audio("audio/Flute/f28.mp3")
 };
-
-const startButton = document.getElementById('#startButton');//instead of using doublequotes
+//const startButton = document.getElementById('#startButton');//instead of using doublequotes
 //startButton.addEventListener("onload", onStart); 
 //console.log(txt);
 
 function playNext() { 
   //txt = document.getElementById("d1").value.toUpperCase()
-  txt="A"//sessionStorage.getItem("usertext").toUpperCase()
+  //txt = document.getElementById("t").value.toUpperCase()
+  txt=sessionStorage.getItem("usertext").toUpperCase()
+  // example=sessionStorage.getItem("usertext").toUpperCase()
+  // let c=sessionStorage.getItem("count")
+  // let k=0
+  // for(let j=c;j<example.length;j++)
+  // {
+  //   txt[k]=example[j]
+  //   k++
+  // }
   if (i < txt.length) { 
     //audio = new Audio(audioArray[audioPointer]);
     if (!(txt[i] in mus)){
@@ -54,6 +56,8 @@ function playNext() {
       mus["#"].addEventListener("ended",playNext);
     }
     else if(txt[i] in mus){
+      console.log(txt);
+      //console.log(c);
       mus[txt[i]].play()
       mus[txt[i]].addEventListener("ended",playNext);
     }
@@ -79,6 +83,3 @@ function onStart() {
   audioPointer = 0;
   playNext();
 }
-   
-   
-
--- js/piano.js
+++ js/piano.js
@@ -2,50 +2,44 @@ let audioPointer;
 let audio;
 let i=0;
 let txt="";
-
-/*
-const audioArray = [ 
-  "b.mp3",
-  "g1" 
-];
-*/
 const mus = {
-  A: new Audio("A0.mp3"),
-  B: new Audio("A1.mp3"),
-  C: new Audio("A2.mp3"),
-  D: new Audio("A3.mp3"),
-  E: new Audio("A4.mp3"),
-  F: new Audio("A5.mp3"),
-  G: new Audio("A6.mp3"),
-  H: new Audio("A7.mp3"),
-  I: new Audio("Ab1.mp3"),
-  J: new Audio("Ab2.mp3"),
-  K: new Audio("Ab3.mp3"),
-  L: new Audio("Ab4.mp3"),
-  M: new Audio("Ab5.mp3"),
-  N: new Audio("Ab6.mp3"),
-  O: new Audio("Ab7.mp3"),
-  P: new Audio("B0.mp3"),
-  Q: new Audio("B1.mp3"),
-  R: new Audio("B2.mp3"),
-  S: new Audio("B3.mp3"),
-  T: new Audio("B4.mp3"),
-  U: new Audio("B5.mp3"),
-  V: new Audio("B6.mp3"),  /*&#119187*/ 
-  W: new Audio("B7.mp3"),
-  X: new Audio("Bb1.mp3"),
-  Y: new Audio("Bb2.mp3"), 
-  Z: new Audio("Bb3.mp3"),
- " ":new Audio("Bb4.mp3"),  //&#119047;	\x1d107
- "#":new Audio("Bb5.mp3")
+  A: new Audio("audio/Piano/A0.mp3"),
+  B: new Audio("audio/Piano/A1.mp3"),
+  C: new Audio("audio/Piano/A2.mp3"),
+  D: new Audio("audio/Piano/A3.mp3"),
+  E: new Audio("audio/Piano/A4.mp3"),
+  F: new Audio("audio/Piano/A5.mp3"),
+  G: new Audio("audio/Piano/A6.mp3"),
+  H: new Audio("audio/Piano/A7.mp3"),
+  I: new Audio("audio/Piano/Ab1.mp3"),
+  J: new Audio("audio/Piano/Ab2.mp3"),
+  K: new Audio("audio/Piano/Ab3.mp3"),
+  L: new Audio("audio/Piano/Ab4.mp3"),
+  M: new Audio("audio/Piano/Ab5.mp3"),
+  N: new Audio("audio/Piano/Ab6.mp3"),
+  O: new Audio("audio/Piano/Ab7.mp3"),
+  P: new Audio("audio/Piano/B0.mp3"),
+  Q: new Audio("audio/Piano/B1.mp3"),
+  R: new Audio("audio/Piano/B2.mp3"),
+  S: new Audio("audio/Piano/B3.mp3"),
+  T: new Audio("audio/Piano/B4.mp3"),
+  U: new Audio("audio/Piano/B5.mp3"),
+  V: new Audio("audio/Piano/B6.mp3"),  /*&#119187*/ 
+  W: new Audio("audio/Piano/B7.mp3"),
+  X: new Audio("audio/Piano/Bb1.mp3"),
+  Y: new Audio("audio/Piano/Bb2.mp3"), 
+  Z: new Audio("audio/Piano/Bb3.mp3"),
+ " ":new Audio("audio/Piano/Bb4.mp3"),  //&#119047;	\x1d107
+ "#":new Audio("audio/Piano/Bb5.mp3")
 };
 
-const startButton = document.getElementById('#startButton');//instead of using doublequotes
+//onst startButton = document.getElementById('#startButton');//instead of using doublequotes
 //startButton.addEventListener("onload", onStart); 
 //console.log(txt);
 
 function playNext() { 
-  txt = document.getElementById("d1").value.toUpperCase()
+  //txt = document.getElementById("d1").value.toUpperCase()
+  txt=sessionStorage.getItem("usertext").toUpperCase()
   if (i < txt.length) { 
     //audio = new Audio(audioArray[audioPointer]);
     if (!(txt[i] in mus)){

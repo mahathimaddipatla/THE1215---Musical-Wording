3403f7a6be2bfaf500e3580930d15ec80272a878
mahathimaddipatla <96947396+mahathimaddipatla@users.noreply.github.com>
2023-01-06 07:00:10 +0530

    Update guitar.js

--- js/guitar.js
+++ js/guitar.js
@@ -10,37 +10,37 @@ const audioArray = [
 ];
 */
 const mus = {
-  A: new Audio("g1.mp3"),
-  B: new Audio("g2.mp3"),
-  C: new Audio("g3.mp3"),
-  D: new Audio("g4.mp3"),
-  E: new Audio("g5.mp3"),
-  F: new Audio("g6.mp3"),
-  G: new Audio("g7.mp3"),
-  H: new Audio("g8.mp3"),
-  I: new Audio("g9.mp3"),
-  J: new Audio("g10.mp3"),
-  K: new Audio("g11.mp3"),
-  L: new Audio("g12.mp3"),
-  M: new Audio("g13.mp3"),
-  N: new Audio("g14.mp3"),
-  O: new Audio("g15.mp3"),
-  P: new Audio("g16.mp3"),
-  Q: new Audio("g17.mp3"),
-  R: new Audio("g18.mp3"),
-  S: new Audio("g19.mp3"),
-  T: new Audio("g20.mp3"),
-  U: new Audio("g21.mp3"),
-  V: new Audio("g22.mp3"),  /*&#119187*/ 
-  W: new Audio("g23.mp3"),
-  X: new Audio("g24.mp3"),
-  Y: new Audio("g25.mp3"), 
-  Z: new Audio("g26.mp3"),
- " ":new Audio("g27.mp3"),  //&#119047;	\x1d107
- "#":new Audio("g12.mp3")
+  A: new Audio("audio/Voilin/v1.mp3"),
+  B: new Audio("audio/Voilin/v2.mp3"),
+  C: new Audio("audio/Voilin/v3.mp3"),
+  D: new Audio("audio/Voilin/v4.mp3"),
+  E: new Audio("audio/Voilin/v5.mp3"),
+  F: new Audio("audio/Voilin/v6.mp3"),
+  G: new Audio("audio/Voilin/v7.mp3"),
+  H: new Audio("audio/Voilin/v8.mp3"),
+  I: new Audio("audio/Voilin/v9.mp3"),
+  J: new Audio("audio/Voilin/v10.mp3"),
+  K: new Audio("audio/Voilin/v11.mp3"),
+  L: new Audio("audio/Voilin/v12.mp3"),
+  M: new Audio("audio/Voilin/v13.mp3"),
+  N: new Audio("audio/Voilin/v14.mp3"),
+  O: new Audio("audio/Voilin/v15.mp3"),
+  P: new Audio("audio/Voilin/v16.mp3"),
+  Q: new Audio("audio/Voilin/v17.mp3"),
+  R: new Audio("audio/Voilin/v18.mp3"),
+  S: new Audio("audio/Voilin/v19.mp3"),
+  T: new Audio("audio/Voilin/v20.mp3"),
+  U: new Audio("audio/Voilin/v21.mp3"),
+  V: new Audio("audio/Voilin/v22.mp3"),  /*&#119187*/ 
+  W: new Audio("audio/Voilin/v23.mp3"),
+  X: new Audio("audio/Voilin/v24.mp3"),
+  Y: new Audio("audio/Voilin/v25.mp3"), 
+  Z: new Audio("audio/Voilin/v26.mp3"),
+ " ":new Audio("audio/Voilin/v27.mp3"),  //&#119047;	\x1d107
+ "#":new Audio("audio/Voilin/v12.mp3")
 };
 
-const startButton = document.getElementById('#startButton');//instead of using doublequotes
+//const startButton = document.getElementById('#startButton');//instead of using doublequotes
 //startButton.addEventListener("onload", onStart); 
 //console.log(txt);
 
